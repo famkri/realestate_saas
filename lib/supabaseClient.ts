f9f3104c4453248cfcde71a300d02679b9c7502c
famkri <i@famkri.com>
2025-07-02 19:48:57 +0200

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createBrowserClient } from '@supabase/auth-helpers-nextjs'
 
 export const supabase = createBrowserClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.https://huxkxnwwobuhqtrkhdlj.supabase.co,
+  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1eGt4bnd3b2J1aHF0cmtoZGxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE0NzY5MzYsImV4cCI6MjA2NzA1MjkzNn0.2E0OP1yvpeadUMj1_2cqmiB11CpYi4-LS41ks9n1Y-s
 )
